f3d662d3bd1ac5a3a5dd246b61e1631fd58a8f46
nz <walksky@189.cn>
2018-09-21 17:11:46 +0800

    modify:add file check and file upload watch

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const multiparty = require('multiparty');
 
 const config = require('./config.js');
+const findSh = require('./find.js')
 
 const user = new Map();
 setInterval(() => user.clear(), 60000 * 60 * 8); //every 8 h clear;
@@ -19,13 +20,16 @@ fs.readdir('./xls', function(err, filename) {
         sheets[name] || (sheets[name] = xls.parse('./xls/' + file)[0].data);
     });
 })
-fs.watch('./xls/', function(event, filename) {
-    setTimeout(function() {
-        if (event == 'rename') sheets[filename.split('.')[0]] ? (delete sheets[filename.split('.')[0]]) : (sheets[filename.split('.')[0]] = xls.parse('./xls/' + filename)[0].data);
-        else sheets[filename.split('.')[0]] = xls.parse('./xls/' + filename)[0].data;
-        console.log(sheets[filename.split('.')[0]]);
-    }, 1000 * 30);
-})
+// fs.watch('./xls/', function(event, filename) {
+//     setTimeout(function() {
+//         if (event == 'rename') {
+//         	if(filename.length>20) return;
+//         	else        	sheets[filename.split('.')[0]] ? (delete sheets[filename.split('.')[0]]) : (sheets[filename.split('.')[0]] = xls.parse('./xls/' + filename)[0].data);
+//         }
+//         else sheets[filename.split('.')[0]] = xls.parse('./xls/' + filename)[0].data;
+//         console.log(sheets[filename.split('.')[0]]);
+//     }, 1000 * 30);
+// })
 
 
 app.listen(5050);
@@ -37,12 +41,28 @@ app.post('/upload', (req, res, next) => {
     let form = new multiparty.Form();
     form.uploadDir = './xls/';
     form.maxFileSize = 10 * 1024 * 1024;
+
     form.parse(req, (err, fields, files) => {
-        fs.rename(files.content[0].path, './xls/' + files.content[0].originalFilename, () => null);
+ 
+    	if(fields.pwd[0]=='nz'){
+    		 fs.rename(files.content[0].path, './xls/' + files.content[0].originalFilename, (err) => {
+            if (err) console.log(err);
+            else {
+                sheets[files.content[0].originalFilename.split('.')[0]] = xls.parse('./xls/' + files.content[0].originalFilename)[0].data;
+
+            }
+        });
+    	}else{
+    		fs.unlink(files.content[0].path,err=>null);
+    	}
+
+       
         res.writeHead(200, { 'content-type': 'text/plain' });
         if (err) res.end('upload failed');
         res.end('upload success');
     });
+
+
 });
 app.use('/wechat', wechat(config, function(req, res, next) {
     //console.log(req.weixin);
@@ -53,7 +73,7 @@ app.use('/wechat', wechat(config, function(req, res, next) {
                 const subText = '欢迎订阅本公号\n回复支局查看经营信息，例如：洋河';
                 const unsubText = '欢迎下次再来！！/::\'(';
                 req.weixin.Event == 'subscribe' ? res.reply(subText) : res.reply(unsubText);
-	break;
+                break;
             }
         case 'text':
             {
@@ -69,42 +89,14 @@ app.use('/wechat', wechat(config, function(req, res, next) {
                     else user.set(funame, user.has(funame) ? user.get(funame) + 1 : 1);
                     if (user.get(funame) > 3) res.reply('查看次数太多/::P');
                     else {
-                        let month = new Date().getMonth();
-                        let msg = req.weixin.Content;
-                        month = msg.replace(/[^0-9]/g, '') || (sheets[month] ? month : month - 1);
-                        if (Object.keys(sheets).indexOf(month + '') == -1) return res.reply('浪费一次机会，请输入正确名称！');
-                        msg = msg.replace(/[0-9]/g, '');
-                        let data = sheets[month].filter(val => {
-                            return val[2] && val[2].includes(msg);
-                        });
-                        if (data.length < 1 || data.length > 2) {
-                            res.reply(`Err:${data.length} 请回复正确的名称/::|`);
-                        } else {
-                            let text = '';
-                            for (let j = 0; j < data.length; j++) {
-                                text += `【${data[j][1]+data[j][3]}】${data[j][4]} \n\n`;
-                                text += '----收入'.padEnd(16, '-');
-                                text += `\nT0:${parseFloat(data[j][5]).toFixed(1)}万 目标:${parseFloat(data[j][6]).toFixed(1)}万\n累计:${parseFloat(data[j][7]).toFixed(1)}万 进度:${parseFloat(data[j][8]).toFixed(1)}%\n增幅:${parseFloat(data[j][9]).toFixed(2)}% 增幅排名:${data[j][10]}\n\n`;
-
-                                text += '----市场竞争'.padEnd(16, '-');
-                                text += `\n天翼到达份额:${parseFloat(data[j][15]).toFixed(1)} 提升:${parseFloat(data[j][17]).toFixed(1)}\n宽带渗透:${parseFloat(data[j][19]).toFixed(1)} 提升:${parseFloat(data[j][20]).toFixed(1)}\n\n`;
-
-                                text += '----今年发展质态'.padEnd(16, '-');
-                                text += `\n天翼活跃:${parseFloat(data[j][21]).toFixed(1)}% 宽带活跃:${parseFloat(data[j][22]).toFixed(1)}%\n\n`;
-
-                                text += '----存量离网率'.padEnd(16, '-');
-                                text += `\n天翼:${parseFloat(data[j][23]).toFixed(1)}% 宽带:${parseFloat(data[j][24]).toFixed(1)}%`;
-
-                            }
-                            res.reply(text);
-                        }
+                        findSh(sheets, req, res);
 
                     }
                 } catch (e) {
                     console.log(e);
                     res.reply('出错了，请重试/::~');
                 }
-	break;
+                break;
             }
         default:
             { res.reply('消息类型不支持'); }
---
+++ find.js
@@ -0,0 +1,61 @@
+/*
+*
+*默认标题是第一旬
+默认查询第三列,
+*最多回复10行数据
+*
+*/
+module.exports = function(sheets,req, res, { row = 0, col = 2 } = {}) {
+    let [namesh,findtxt]= req.weixin.Content.replace(/[\s\.，]+/g,' ').split(' ');
+ 
+    let month = namesh.replace(/[^0-9]+/g, '') || (sheets[new Date().getMonth()] ? new Date().getMonth() : new Date().getMonth() - 1);
+    let flag=true;
+    if(!findtxt) {
+    
+    	findtxt=namesh.replace(/[0-9]+/g,'');
+    	namesh=month;
+    	flag=false;
+    	}
+    if (Object.keys(sheets).indexOf(namesh + '') == -1) return res.reply('浪费一次机会，请输入正确名称！');
+    let data = sheets[namesh].filter(val => {
+    	console.log('val:'+val[col]);
+        return val[col] && (''+val[col]).includes(findtxt);
+    });
+    console.log('\n'+data);
+    if (data.length < 1 || data.length > 10) {
+        res.reply(`Err:${data.length} 请回复正确的名称/::|`);
+    } else {
+        let msg = '';
+        if (flag) {
+            let count = 0;
+            for (let j = 0; j < data.length; j++) {
+                for (let i = 0; i < data[j].length; i++) {
+                    if (data[j][i]) {
+                        msg += `${data[row][i]}: ${data[j][i]}	`;
+                        count++;
+                        if (count % 2 == 0) msg += '\n';
+                    }
+                }
+
+            }
+        } else {
+            for (let j = 0; j < data.length; j++) {
+                msg += `【${data[j][1]+data[j][3]}】${data[j][4]} \n\n`;
+                msg += '----收入'.padEnd(16, '-');
+                msg += `\nT0:${parseFloat(data[j][5]).toFixed(1)}万 目标:${parseFloat(data[j][6]).toFixed(1)}万\n累计:${parseFloat(data[j][7]).toFixed(1)}万 进度:${parseFloat(data[j][8]).toFixed(1)}%\n增幅:${parseFloat(data[j][9]).toFixed(2)}% 增幅排名:${data[j][10]}\n\n`;
+
+                msg += '----市场竞争'.padEnd(16, '-');
+                msg += `\n天翼到达份额:${parseFloat(data[j][15]).toFixed(1)} 提升:${parseFloat(data[j][17]).toFixed(1)}\n宽带渗透:${parseFloat(data[j][19]).toFixed(1)} 提升:${parseFloat(data[j][20]).toFixed(1)}\n\n`;
+
+                msg += '----今年发展质态'.padEnd(16, '-');
+                msg += `\n天翼活跃:${parseFloat(data[j][21]).toFixed(1)}% 宽带活跃:${parseFloat(data[j][22]).toFixed(1)}%\n\n`;
+
+                msg += '----存量离网率'.padEnd(16, '-');
+                msg += `\n天翼:${parseFloat(data[j][23]).toFixed(1)}% 宽带:${parseFloat(data[j][24]).toFixed(1)}%`;
+
+            }
+        }
+
+        res.reply(msg);
+    }
+}
